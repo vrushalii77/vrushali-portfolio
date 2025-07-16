1c160ef1d54b78195154a985dbe3583cc43b2acf
jadhavvn <vrushalinjadhav123@gmail.com>
2025-07-16 17:16:17 +0530

    changed the icons in intro page

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  FaPaperPlane,
+  FaLaptopCode,
+  FaUser,
+  FaBriefcase,
+} from "react-icons/fa";
+
 import "./Intro.css";
 
 function Intro() {
@@ -23,55 +30,19 @@ function Intro() {
         <div className="btn-content">
           <Link to="/Contact">
             <button className="hireme-btn">
-              <img className="hireme-img" src="./hireme.png" alt="hire me" />
+              <FaPaperPlane className="hireme-icon" />
               Get In Touch
             </button>
           </Link>
           <Link to="/Project">
             <button className="hireme-btn">
-              <img className="hireme-img" src="./hireme.png" alt="hire me" />
+              <FaLaptopCode className="hireme-icon" />
               View Projects
             </button>
           </Link>
         </div>
-        {/* <div className="intro-img">
-        <img className="selfimg" src="./VrushaliSelfPic.jpg" alt="Vrushali" />
-      </div> */}
       </section>
-      {/* <section id="about">
-        <span className="about-heading">About Me</span>
 
-        <div className="about-box">
-          <div className="abt-me">
-            <span className="i-am">Who I Am</span>
-            <span className="about-description">
-              I am Vrushali Jadhav, a passionate and dedicated Java Full Stack
-              Developer with a strong foundation in both front-end and back-end
-              development. I specialize in building user-friendly, responsive,
-              and performance-driven web applications. With hands-on experience
-              in technologies like Java, Spring Boot, React, MySQL, and more.
-            </span>
-            <span className="about-description">
-              I enjoy transforming ideas into real-world solutions. I am
-              continuously learning, exploring new tech, and striving to grow as
-              a developer every day. My goal is to contribute to impactful
-              projects and be a part of innovative teams that value clean code
-              and creative problem-solving
-            </span>
-            <div className="abt-btn">
-              <Link to="/aboutme">
-                <button className="abt-btn1">More About Me</button>
-              </Link>
-              <Link to="/project">
-                <button className="abt-btn1">See my work</button>
-              </Link>
-            </div>
-          </div>
-          <div className="about-img">
-            <img src="./girl2.png" alt="girl with laptop" />
-          </div>
-        </div>
-      </section> */}
       <section id="about">
         <span className="about-heading">About Me</span>
 
@@ -98,10 +69,16 @@ function Intro() {
               </span>
               <div className="abt-btn">
                 <Link to="/aboutme">
-                  <button className="abt-btn1">More About Me</button>
+                  <button className="abt-btn1">
+                    <FaUser className="hireme-icon" />
+                    More About Me
+                  </button>
                 </Link>
                 <Link to="/project">
-                  <button className="abt-btn1">See my work</button>
+                  <button className="abt-btn1">
+                    <FaBriefcase className="hireme-icon" />
+                    See my work
+                  </button>
                 </Link>
               </div>
             </div>
